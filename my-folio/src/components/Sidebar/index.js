6fbe084996fcc73dcf40b89381c811f75767a37f
Ade <aderitchards@gmail.com>
2022-10-20 18:09:17 -0700

    Working Nav bar progress

--- my-folio/src/components/Sidebar/index.js
+++ my-folio/src/components/Sidebar/index.js
@@ -9,8 +9,25 @@ const Sidebar = () =>(
     <div className='nav-bar'>
         <Link className='logo' to='/'>
             <img src={logo} alt="logo"/>
-            <img src={logoText} alt='logo text' />
+            <img className="sub-logo" src={logoText} alt='logo text' />
         </Link>
+        <nav className='nav-nav'>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+            </NavLink>
+
+            <NavLink exact="true" 
+            className="about-link"
+            activeclassname="active" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
+            </NavLink>
+
+            <NavLink exact="true" 
+            className="contact-link"
+            activeclassname="active" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
+            </NavLink>
+        </nav>
     </div>
 )
 
